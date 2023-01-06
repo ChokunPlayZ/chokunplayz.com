1ad1c4ae62443a2908f3627fe9313b804f7db1e1
Chokun Rojanapron <chokun@chokunplayz.com>
2023-01-06 21:07:19 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Head>
+      <Title>ChokunPlayZ | Index</Title>
+    </Head>
+    <div>
+      <h1>Hello!</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
